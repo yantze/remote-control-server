87ff7a4d5a6fa4b9502ac46d1619cca05e3395e6
yantze <yantze@126.com>
2021-01-24 16:02:37 +0800

    chore: 提交编译后代码

---
+++ dist/precache-manifest.79b04e1cf4bf8c4a2024c53f71c1c20f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "231147fb2a8bae6e9697",
+    "url": "/css/chunk-vendors.d6381dee.css"
+  },
+  {
+    "revision": "231147fb2a8bae6e9697",
+    "url": "/js/chunk-vendors.60c9e85b.js"
+  },
+  {
+    "revision": "e83a70d99530591d5ba6",
+    "url": "/css/index.3919b411.css"
+  },
+  {
+    "revision": "e83a70d99530591d5ba6",
+    "url": "/js/index.1096b474.js"
+  },
+  {
+    "revision": "42ab717ed262e7e36daaec8d20b3541e",
+    "url": "/index.html"
+  }
+];
